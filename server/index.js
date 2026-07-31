38df6bf481295ad1b91474dfbc90cfbf33323677
vincenttw <vincenttw@moregeek.com>
2026-07-31 15:44:25 +0800

    Allow external access to admin panel login screen on Render

--- server/index.js
+++ server/index.js
@@ -173,9 +173,6 @@ function isAllowedAdminSource(req) {
 }
 
 function handleAdminPageServe(req, res) {
-  if (!isLocalhostRequest(req)) {
-    return res.status(403).send('Access Denied: Admin panel is strictly restricted to localhost connection.');
-  }
   const adminPath = path.join(__dirname, 'admin.html');
   if (fs.existsSync(adminPath)) {
     res.sendFile(adminPath);
